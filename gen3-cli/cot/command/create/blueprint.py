ca83113593e37462f564dc6bb07e03e299e09f16
Neketek <neketek@gmail.com>
2019-11-10 21:31:18 +0200

    createBlueprint.sh wrapper

--- gen3-cli/cot/command/create/blueprint.py
+++ gen3-cli/cot/command/create/blueprint.py
@@ -1,6 +1,15 @@
 import click
+from .template import template
 
 
-@click.command()
-def blueprint():
-    pass
+@click.command(
+    context_settings=dict(
+        allow_extra_args=True,
+        ignore_unknown_options=True
+    )
+)
+@click.pass_context
+def blueprint(ctx, **kwargs):
+    ctx.forward(template, level='blueprint')
+
+blueprint.params = list(filter(lambda p: p.name!='level', template.params))
