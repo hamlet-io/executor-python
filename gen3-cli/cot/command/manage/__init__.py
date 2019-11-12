4b7fa8ed265dcf79edae5791dc60dacfcd4da284
Neketek <neketek@gmail.com>
2019-11-12 19:12:27 +0200

    manageFileCrypto.sh wrapper

--- gen3-cli/cot/command/manage/__init__.py
+++ gen3-cli/cot/command/manage/__init__.py
@@ -2,6 +2,7 @@ from cot.command import root as cli
 from .stack import stack
 from .deployment import deployment
 from .crypto import crypto
+from .file_crypto import file_crypto
 
 
 @cli.group('manage')
@@ -14,3 +15,4 @@ def group():
 group.add_command(deployment)
 group.add_command(stack)
 group.add_command(crypto)
+group.add_command(file_crypto)
