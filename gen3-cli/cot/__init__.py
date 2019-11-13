d56544320398f2e72cb75b075f0e3617cff801e6
Neketek <neketek@gmail.com>
2019-11-13 20:43:58 +0200

    runSentryRelease.py

--- gen3-cli/cot/__init__.py
+++ gen3-cli/cot/__init__.py
@@ -2,6 +2,6 @@
 # We can do it in command.__init__ but I guess this is a little bit more explicit
 import cot.command  # noqa
 import cot.command.create  # noqa
-import cot.command.add  # noqa
+# import cot.command.add  # noqa
 import cot.command.manage  # noqa
 import cot.command.run  # noqa
---
+++ gen3-cli/cot/command/run/sentry_release.py
@@ -0,0 +1,58 @@
+import subprocess
+import click
+from cot import utils
+from cot import env
+
+
+@click.command(
+    'sentry-release',
+    context_settings=dict(
+        max_content_width=240
+    )
+)
+@click.option(
+    '-m',
+    '--sentry-source-map-s3-url',
+    required=True,
+    help='s3 link to sourcemap files'
+)
+@click.option(
+    '-p',
+    '--sentry-url-prefix',
+    help='prefix for sourcemap files'
+)
+@click.option(
+    '-r',
+    '--sentry-release-name',
+    help='sentry release name',
+    required=True
+)
+@click.option(
+    '-s',
+    '--run-setup',
+    help='run setup installation to prepare',
+    is_flag=True
+)
+def sentry_release(
+    sentry_source_map_s3_url,
+    sentry_url_prefix,
+    sentry_release_name,
+    run_setup
+):
+    """
+    Upload sourcemap files to sentry for a specific release
+    """
+    script_call_line = utils.cli_params_to_script_call(
+        env.GENERATION_DIR,
+        'runSentryRelease.sh',
+        options={
+            '-m': sentry_source_map_s3_url,
+            '-p': sentry_url_prefix,
+            '-r': sentry_release_name,
+            '-s': run_setup
+        }
+    )
+    subprocess.run(
+        script_call_line,
+        shell=True
+    )
