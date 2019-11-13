f4d379bed878f0da923b758671d33b7bf3ecfab3
Neketek <neketek@gmail.com>
2019-11-13 20:43:58 +0200

    runSentryRelease.py

--- gen3-cli/cot/command/run/__init__.py
+++ gen3-cli/cot/command/run/__init__.py
@@ -3,6 +3,7 @@ from .expo_app_publish import expo_app_publish
 from .task import task
 from .lambda_func import lambda_func
 from .pipeline import pipeline
+from .sentry_release import sentry_release
 
 
 @cli.group('run')
@@ -16,3 +17,4 @@ group.add_command(expo_app_publish)
 group.add_command(task)
 group.add_command(lambda_func)
 group.add_command(pipeline)
+group.add_command(sentry_release)
