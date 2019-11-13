3adab84087fc762dd0be9a2e526210da1dbd460d
Neketek <neketek@gmail.com>
2019-11-13 16:59:37 +0200

    runExpoAppPublish.sh wrapper

--- gen3-cli/cot/command/run/__init__.py
+++ gen3-cli/cot/command/run/__init__.py
@@ -1,4 +1,5 @@
 from cot.command import root as cli
+from .expo_app_publish import expo_app_publish
 
 
 @cli.group('run')
@@ -6,3 +7,6 @@ def group():
     """
     Runs stuff
     """
+
+
+group.add_command(expo_app_publish)
