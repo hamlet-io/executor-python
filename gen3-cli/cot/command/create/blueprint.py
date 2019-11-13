b40a9c2c533a714bd4821ce38c7fa05679a35555
Neketek <neketek@gmail.com>
2019-11-13 15:44:46 +0200

    Updated docs for create blueprint and create build-blueprint

--- gen3-cli/cot/command/create/blueprint.py
+++ gen3-cli/cot/command/create/blueprint.py
@@ -5,12 +5,32 @@ from .template import template
 @click.command(
     context_settings=dict(
         allow_extra_args=True,
-        ignore_unknown_options=True
+        ignore_unknown_options=True,
+        max_content_width=240
     )
 )
 @click.pass_context
 def blueprint(ctx, **kwargs):
+    """
+    Create a blueprint output of the segment
+
+    \b
+    NOTES:
+
+    1. You must be in the directory specific to the level
+    """
     ctx.forward(template, level='blueprint')
 
 
-blueprint.params = list(filter(lambda p: p.name != 'level', template.params))
+def filter_params(option):
+    return option.name in [
+        'generation_input_source',
+        'generation_framework',
+        'generation_provider',
+        'generation_testcase',
+        'generation_scenarios',
+        'deployment_unit'
+    ]
+
+
+blueprint.params = list(filter(filter_params, template.params))
--- gen3-cli/cot/command/create/build_blueprint.py
+++ gen3-cli/cot/command/create/build_blueprint.py
@@ -6,12 +6,31 @@ from .template import template
     'build-blueprint',
     context_settings=dict(
         allow_extra_args=True,
-        ignore_unknown_options=True
+        ignore_unknown_options=True,
+        max_content_width=240
     )
 )
 @click.pass_context
 def build_blueprint(ctx, **kwargs):
+    """
+    Create a blueprint output for the provided deployment unit
+
+    \b
+    NOTES:
+
+    1. You must be in the directory specific to the level
+    """
     ctx.forward(template, level='buildblueprint')
 
 
-build_blueprint.params = list(filter(lambda p: p.name != 'level', template.params))
+def filter_params(option):
+    return option.name in [
+        'generation_input_source',
+        'generation_framework',
+        'generation_scenarios',
+        'generation_testcase',
+        'deployment_unit'
+    ]
+
+
+build_blueprint.params = list(filter(filter_params, template.params))
