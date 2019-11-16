3afdaa7f773cf1b22ebb31e89b0009279ef243e7
Neketek <neketek@gmail.com>
2019-11-16 18:32:56 +0200

    Options generator required options support added

--- gen3-cli/tests/unit/command/test_option_generation.py
+++ gen3-cli/tests/unit/command/test_option_generation.py
@@ -3,10 +3,10 @@ import pytest
 from cot.loggers import logging
 
 
-logger = logging.getLogger('OPTIONS GEN')
+logger = logging.getLogger('options')
 
 
-def generate_test_options_collection(options):
+def generate_test_options_collection(all_options):
     """
     Generates collection of CLI options based on OrderedDict with next structure:
         ordered_dict['-o, --option'] = 'value'
@@ -32,11 +32,18 @@ def generate_test_options_collection(options):
     iterable collection values and then creates variations by iterating over one of the keys with collection values
     untill all of them iterated.
     """
-    max_keys_parts = 1
-    collection_values_keys = []
+    required_options = collections.OrderedDict()
+    all_keys_max_parts = 1
+    required_keys_max_parts = 1
+    required_keys_collection_values = []
+    optional_keys_collection_values = []
+
+    def are_required_keys(keys):
+        return keys.startswith('!')
 
     def parse_keys(keys):
-        return tuple(key.strip() for key in keys.split(',') if key)
+        # remoing spaces and required flag
+        return tuple(key.strip().strip('!') for key in keys.split(',') if key)
 
     def get_key(keys, index):
         keys = parse_keys(keys)
@@ -48,15 +55,21 @@ def generate_test_options_collection(options):
     def is_values_collection(values):
         return isinstance(values, collections.abc.Iterable) and not isinstance(values, str)
 
-    for keys in options:
-        max_keys_parts = max(max_keys_parts, len(parse_keys(keys)))
-
-    for keys, values in options.items():
+    for keys, values in all_options.items():
+        key_parts = parse_keys(keys)
+        all_keys_max_parts = max(all_keys_max_parts, len(key_parts))
+        required = are_required_keys(keys)
+        if required:
+            required_options[keys] = values
+            required_keys_max_parts = max(required_keys_max_parts, len(key_parts))
         if is_values_collection(values):
-            collection_values_keys.append(keys)
+            if required:
+                required_keys_collection_values.append(keys)
+            else:
+                optional_keys_collection_values.append(keys)
 
     # fixed_value_keys is plural because it's a commaseparated list of keys
-    def generate_args(key_index, fixed_value_keys=None, fixed_value_index=0):
+    def generate_args(key_index, fixed_value_keys=None, fixed_value_index=0, options=None):
         args = []
         str_args = []
         for keys, values in options.items():
@@ -88,24 +101,74 @@ def generate_test_options_collection(options):
             )
         return args
 
-    for key_index in range(max_keys_parts):
-        yield generate_args(key_index)
-    # starting from index 1 because previous generation conains all 0 indexes
-    if collection_values_keys:
-        for fixed_value_keys in collection_values_keys:
-            # logger.info('Fixed keys:%s', fixed_value_keys)
-            for fixed_value_index in range(1, len(options[fixed_value_keys])):
-                for key_index in range(max_keys_parts):
-                    yield generate_args(key_index, fixed_value_keys, fixed_value_index)
+    def generate_args_group(max_keys_parts, options, collection_values_keys):
+        for key_index in range(max_keys_parts):
+            yield generate_args(key_index, options=options)
+        # starting from index 1 because previous generation conains all 0 indexes
+        if collection_values_keys:
+            for fixed_value_keys in collection_values_keys:
+                for fixed_value_index in range(1, len(options[fixed_value_keys])):
+                    for key_index in range(max_keys_parts):
+                        yield generate_args(
+                            key_index,
+                            fixed_value_keys,
+                            fixed_value_index,
+                            options
+                        )
+    # generating using only required options
+    if required_options and len(required_options) != len(all_options):
+        logger.info('[required]')
+        for args in generate_args_group(
+            required_keys_max_parts,
+            required_options,
+            required_keys_collection_values
+        ):
+            yield args
+
+    # generating using all options
+    logger.info('[all]')
+    for args in generate_args_group(
+        all_keys_max_parts,
+        all_options,
+        optional_keys_collection_values
+    ):
+        yield args
 
 
 def test_option_generation():
     options = collections.OrderedDict()
-    options['-a,--a'] = 'avalue'
+    options['!-a,--a'] = 'avalue'
     options['-b,--b'] = ['bvalue1', 'bvalue2']
-    options['-c,--c'] = ['cvalue1', 'cvalue2', 'cvalue3']
+    options['!-c,--c'] = ['cvalue1', 'cvalue2', 'cvalue3']
     options['-f,--f'] = [True, False]
     generator = generate_test_options_collection(options)
+    # start with  only required options
+    assert next(generator) == [
+        '-a', 'avalue',
+        '-c', 'cvalue1'
+    ]
+    assert next(generator) == [
+        '--a', 'avalue',
+        '--c', 'cvalue1'
+    ]
+    # iteration over required collection values
+    assert next(generator) == [
+        '-a', 'avalue',
+        '-c', 'cvalue2'
+    ]
+    assert next(generator) == [
+        '--a', 'avalue',
+        '--c', 'cvalue2'
+    ]
+    assert next(generator) == [
+        '-a', 'avalue',
+        '-c', 'cvalue3'
+    ]
+    assert next(generator) == [
+        '--a', 'avalue',
+        '--c', 'cvalue3'
+    ]
+    # then all options without required collection values
     assert next(generator) == [
         '-a', 'avalue',
         '-b', 'bvalue1',
@@ -130,31 +193,6 @@ def test_option_generation():
         '--a', 'avalue',
         '--b', 'bvalue2',
         '--c', 'cvalue1',
-        '--f'
-    ]
-    # iterating over '-c,--c' collection
-    assert next(generator) == [
-        '-a', 'avalue',
-        '-b', 'bvalue1',
-        '-c', 'cvalue2',
-        '-f'
-    ]
-    assert next(generator) == [
-        '--a', 'avalue',
-        '--b', 'bvalue1',
-        '--c', 'cvalue2',
-        '--f'
-    ]
-    assert next(generator) == [
-        '-a', 'avalue',
-        '-b', 'bvalue1',
-        '-c', 'cvalue3',
-        '-f'
-    ]
-    assert next(generator) == [
-        '--a', 'avalue',
-        '--b', 'bvalue1',
-        '--c', 'cvalue3',
         '--f'
     ]
     # iterating over '-f,--f'
