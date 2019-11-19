ad22bfefa89339b274b60272dad5baae20a8c063
Neketek <neketek@gmail.com>
2019-11-19 21:47:39 +0200

    Prod version freeze + missed noqa

--- gen3-cli/cot/__init__.py
+++ gen3-cli/cot/__init__.py
@@ -1,7 +1,7 @@
 # Here we controll modules loading order
 # We can do it in command.__init__ but I guess this is a little bit more explicit
 import cot.command  # noqa
-import cot.command.generate
+import cot.command.generate  # noqa
 import cot.command.create  # noqa
 # import cot.command.add  # noqa
 import cot.command.manage  # noqa
