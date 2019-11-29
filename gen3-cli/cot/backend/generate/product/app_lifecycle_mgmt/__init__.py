5df7b30cd586b2046191b68aee0e9de438164f38
Neketek <neketek@gmail.com>
2019-11-29 12:56:53 +0200

    Fixes + Improvements:
    1. create blueprint deployment_unit option removed
    2. create template blueprint level option removed
    3. generalization of the generation backend

--- gen3-cli/cot/backend/generate/product/app_lifecycle_mgmt/__init__.py
+++ gen3-cli/cot/backend/generate/product/app_lifecycle_mgmt/__init__.py
@@ -1,24 +1,5 @@
-import os
-from cookiecutter.main import cookiecutter
-from cot import conf
 from cot.backend.generate import utils
 
 
-TEMPLATES_DIR = os.path.join(conf.COOKIECUTTER_TEMPLATES_BASE_DIR, 'products')
-
-
 def run(**kwargs):
-    utils.replace_parameters_values(
-        kwargs,
-        [
-            [None, ''],
-            [True, 'yes'],
-            [False, 'no']
-        ]
-    )
-    template_path = os.path.join(TEMPLATES_DIR, 'app-lifecycle-mgmt')
-    cookiecutter(
-        template_path,
-        no_input=True,
-        extra_context=kwargs
-    )
+    utils.cookiecutter('products', 'app-lifecycle-mgmt', **kwargs)
--- gen3-cli/cot/backend/generate/utils.py
+++ gen3-cli/cot/backend/generate/utils.py
@@ -1,6 +1,28 @@
+import os
+from cookiecutter.main import cookiecutter as cookiecutter_main
+from cot import conf
+
+
 def replace_parameters_values(kwargs, replacers=None):
     for key, value in kwargs.items():
         for target, replacer in replacers:
             if value is target or value == target:
                 kwargs[key] = replacer
                 break
+
+
+def cookiecutter(*template_path, **kwargs):
+    replace_parameters_values(
+        kwargs,
+        [
+            [None, ''],
+            [True, 'yes'],
+            [False, 'no']
+        ]
+    )
+    template_path = os.path.join(conf.COOKIECUTTER_TEMPLATES_BASE_DIR, *template_path)
+    cookiecutter_main(
+        template_path,
+        no_input=True,
+        extra_context=kwargs
+    )
