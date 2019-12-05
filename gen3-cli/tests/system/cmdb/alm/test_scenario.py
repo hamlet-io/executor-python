da6004137d0ec05e87357033554cc0bf55f74b0f
Neketek <neketek@gmail.com>
2019-12-05 17:59:16 +0200

    CMDB generation + broken test

--- gen3-cli/tests/system/cmdb/alm/test_scenario.py
+++ gen3-cli/tests/system/cmdb/alm/test_scenario.py
@@ -1,8 +1,8 @@
-# from . import step_1_create_cmdb
-# from . import step_2_create_account_templates
-# from . import step_3_create_soultion_templates
 from . import step_1_create_cmdb
+# from . import step_2_create_account_templates
 
 
 def test(cmdb, clear_cmdb):
-    step_1_create_cmdb.test_cmdb_creation(cmdb, clear_cmdb)
+    step_1_create_cmdb.run(cmdb, clear_cmdb)
+    # test fails for unknown reason, doesn't create files in specified directories
+    # step_2_create_account_templates.run(cmdb)
