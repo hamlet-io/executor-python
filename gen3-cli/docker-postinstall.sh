1cbf301f50221a798ec4102d8e87db5da1f67da8
Neketek <neketek@gmail.com>
2019-12-06 22:03:25 +0200

    cot-testcase command

---
+++ gen3-cli/docker-postinstall.sh
@@ -0,0 +1,6 @@
+# stash files to prevent deletion when host volume mounts
+# useful when you want to break stuff using your favorite editor
+# which is not vim or emac.
+mv /opt/codeontap /codeontap
+# make a command from a test module, just for demo purposes
+ln -s /gen3-cli/cot-testcase.sh /bin/cot-testcase
