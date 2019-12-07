f97b443dc0929a01bdf8d75e4ef8528e5d48215f
Neketek <neketek@gmail.com>
2019-12-07 14:51:15 +0200

    cfn-flip freeze + some docker scripts verbosity

--- gen3-cli/docker-cmd.sh
+++ gen3-cli/docker-cmd.sh
@@ -1,7 +1,13 @@
 # clearing previos version
+echo "Current /opt/codeontap volume state"
+tree -L 2 /opt
+echo "Removing old files"
 rm -rf /opt/codeontap/*
 # moving stashed version to GENERATION_DIR
+echo "Moving actual files back into /opt/codeontap"
 mv /codeontap/* /opt/codeontap
+echo "Changing files mode to 777"
 chmod -R 777 /opt/codeontap
 # just waiting forever
+echo "Waiting forever..."
 tail -f -s 60 /dev/null
