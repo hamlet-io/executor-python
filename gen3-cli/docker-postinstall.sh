563692b05f8364f08788c4056fbe2dd0e727f291
Neketek <neketek@gmail.com>
2019-12-07 14:51:15 +0200

    cfn-flip freeze + some docker scripts verbosity

--- gen3-cli/docker-postinstall.sh
+++ gen3-cli/docker-postinstall.sh
@@ -1,6 +1,10 @@
 # stash files to prevent deletion when host volume mounts
 # useful when you want to break stuff using your favorite editor
 # which is not vim or emac.
+echo "Moving /opt/codeontap => /codeontap to prevent overriding by volume"
 mv /opt/codeontap /codeontap
+echo "Current /codeontap content"
+tree /codeontap -L 2
 # make a command from a test module, just for demo purposes
+echo "Creating cot-tescase bin file"
 ln -s /gen3-cli/cot-testcase.sh /bin/cot-testcase
