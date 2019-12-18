4c3f44f5f24c15d07bc4b74ac45bf7760e31d059
Neketek <neketek@gmail.com>
2019-12-18 20:11:25 +0200

    CF test template renderer

---
+++ gen3-cli/cot/backend/tests/templates_loader.py
@@ -0,0 +1,14 @@
+import os
+from jinja2 import Environment, FileSystemLoader
+
+TEMPLATES_DIR = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    'templates'
+)
+
+
+loader = Environment(
+    loader=FileSystemLoader(TEMPLATES_DIR),
+    trim_blocks=True,
+    lstrip_blocks=True
+)
