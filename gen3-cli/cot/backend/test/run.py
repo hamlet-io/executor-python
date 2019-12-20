8797e5c28d5afda671a7a9d0d2ac2c3b55ead037
Neketek <neketek@gmail.com>
2019-12-20 17:17:44 +0200

    pytest generation and run cmds improvements and full tests

--- gen3-cli/cot/backend/test/run.py
+++ gen3-cli/cot/backend/test/run.py
@@ -1,16 +1,33 @@
-import pytest
-from pytest import ExitCode
+import subprocess
+from pytest import ExitCode as ec
 
 
 def run(*testpaths):
-    args = [
-        '--cache-clear',
-        *testpaths,
-    ]
-    exit_code = pytest.main(args)
-    if exit_code in [ExitCode.OK, ExitCode.NO_TESTS_COLLECTED]:
-        return True
-    elif exit_code == ExitCode.TESTS_FAILED:
-        return False
-    elif exit_code in [ExitCode.INTERRUPTED, ExitCode.USAGE_ERROR, ExitCode.INTERNAL_ERROR]:
-        raise Exception(str(exit_code))
+    # Do not use pytest.main because it can't correctly work with the files changed in runtime
+    try:
+        args = [
+            'pytest',
+            '-vv',
+            '--cache-clear',
+            *testpaths,
+        ]
+        cmd = " ".join(args)
+        process = subprocess.Popen(
+            [
+                '/bin/bash',
+                '-c',
+                cmd
+            ],
+            start_new_session=True
+        )
+        process.wait()
+        if process.returncode in [ec.OK, ec.NO_TESTS_COLLECTED]:
+            return True
+        if process.returncode == ec.TESTS_FAILED:
+            return False
+        elif process.returncode in [ec.INTERNAL_ERROR, ec.USAGE_ERROR]:
+            raise Exception(process.stderr)
+        else:
+            raise Exception("Unknown exit code: %s" % process.returncode)
+    finally:
+        process.kill()
