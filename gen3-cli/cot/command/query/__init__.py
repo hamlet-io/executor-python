e657c3726901e44d959a025512ee96b62fe382f4
Neketek <neketek@gmail.com>
2020-01-25 20:39:02 +0200

    cot query command v2

--- gen3-cli/cot/command/query/__init__.py
+++ gen3-cli/cot/command/query/__init__.py
@@ -1,70 +1,136 @@
 import os
 import json
+import textwrap
 import click
+from tabulate import tabulate
 from cot.backend.common import exceptions
 from cot.backend import query as query_backend
 from cot.command import root
 
 
-@root.command(
-    'query',
-    context_settings=dict(
-        max_content_width=240
+MAX_TABLE_TEXT_CONTENT_WIDTH = 128
+
+
+@root.group('query')
+def query():
+    pass
+
+
+def blueprint_generation_options(func):
+    options = []
+    options.append(
+        click.option(
+            '-i',
+            '--blueprint-generation-input-source',
+            help='source of input data to use when generating the template'
+        )
     )
-)
-@click.option(
-    '-i',
-    '--blueprint-generation-input-source',
-    help='source of input data to use when generating the template'
-)
-@click.option(
-    '-p',
-    '--blueprint-generation-provider',
-    help='provider to for template generation',
-    default='aws',
-    show_default=True
-)
-@click.option(
-    '-f',
-    '--blueprint-generation-framework',
-    help='output framework to use for template generation',
-    default='cf',
-    show_default=True
-)
-@click.option(
-    '-s',
-    '--blueprint-generation-scenarios',
-    help='comma seperated list of framework scenarios to load'
-)
+    options.append(
+        click.option(
+            '-p',
+            '--blueprint-generation-provider',
+            help='provider to for template generation',
+            default='aws',
+            show_default=True
+        )
+    )
+    options.append(
+        click.option(
+            '-f',
+            '--blueprint-generation-framework',
+            help='output framework to use for template generation',
+            default='cf',
+            show_default=True
+        )
+    )
+    options.append(
+        click.option(
+            '-s',
+            '--blueprint-generation-scenarios',
+            help='comma seperated list of framework scenarios to load'
+        )
+    )
+    options.append(
+        click.option(
+            '-r',
+            '--blueprint-refresh',
+            is_flag=True,
+            help='force refresh blueprint'
+        )
+    )
+    for decorator in options[::-1]:
+        func = decorator(func)
+    return func
+
+
+@query.command('get')
 @click.option(
     '-q',
     '--query',
-    help='JMESPath query'
+    help='JMESPath query',
+    required=True
 )
-@click.option(
-    '-t',
-    '--list-tiers',
-    is_flag=True,
-    help='print a table of blueprint tiers'
-)
-@click.option(
-    '-c',
-    '--list-components',
-    is_flag=True,
-    help='print a table of blueprint components'
-)
-@click.option(
-    '-r',
-    '--blueprint-refresh',
-    is_flag=True,
-    help='force refresh blueprint'
-)
+@blueprint_generation_options
 @exceptions.handler()
-def query(**kwargs):
+def get(**kwargs):
     result = query_backend.run(**kwargs, cwd=os.getcwd(), _is_cli=True)
-    for key, value in result.items():
-        if len(result) > 1:
-            click.echo(key.upper())
-        if key == 'query':
-            value = json.dumps(value, indent=4)
-        click.echo(value)
+    click.echo(json.dumps(result.get('query'), indent=4))
+
+
+@query.command('list-tiers')
+@blueprint_generation_options
+@exceptions.handler()
+def list_tiers(**kwargs):
+    result = query_backend.run(**kwargs, list_tiers=True, cwd=os.getcwd(), _is_cli=True)
+    click.echo(tiers_table(result['tiers']))
+
+
+@query.command('list-components')
+@blueprint_generation_options
+@exceptions.handler()
+def list_components(**kwargs):
+    result = query_backend.run(**kwargs, list_components=True, cwd=os.getcwd(), _is_cli=True)
+    click.echo(components_table(result['components']))
+
+
+def wrap_text(text):
+    if text is None:
+        return "None"
+    return "\n".join(textwrap.wrap(text, MAX_TABLE_TEXT_CONTENT_WIDTH))
+
+
+def tiers_table(data):
+    tablerows = []
+    for row in data:
+        tablerows.append(
+            [
+                wrap_text(row['Id']),
+                wrap_text(row['Name']),
+                wrap_text(row['Description']),
+                row['NetworkEnabledState']
+            ]
+        )
+    return tabulate(
+        tablerows,
+        headers=['Id', 'Name', 'Description', 'NetworkEnabledState'],
+        showindex=True,
+        tablefmt="fancy_grid"
+    )
+
+
+def components_table(data):
+    tablerows = []
+    for row in data:
+        tablerows.append(
+            [
+                wrap_text(row['Id']),
+                wrap_text(row['Type']),
+                wrap_text(row['Name'])
+            ]
+        )
+    return tabulate(
+        tablerows,
+        headers=['Id', 'Type', 'Name'],
+        showindex=True,
+        tablefmt="fancy_grid"
+    )
--- gen3-cli/tests/unit/command/query/test.py
+++ gen3-cli/tests/unit/command/query/test.py
@@ -1,22 +1,35 @@
+import copy
 import collections
 from unittest import mock
 from click.testing import CliRunner
-from cot.command.query import query
+from cot.command.query import get, list_tiers, list_components
 from tests.unit.command.test_option_generation import run_options_test
 
 
-ALL_VALID_OPTIONS = collections.OrderedDict()
+COMMON_VALID_OPTIONS = collections.OrderedDict()
 
-ALL_VALID_OPTIONS['-p,--blueprint-generation-provider'] = 'generation_provider'
-ALL_VALID_OPTIONS['-f,--blueprint-generation-framework'] = 'generation_framework'
-ALL_VALID_OPTIONS['-i,--blueprint-generation-input-source'] = 'input-source'
-ALL_VALID_OPTIONS['-s,--blueprint-generation-scenarios'] = 'generation_scenarious'
-ALL_VALID_OPTIONS['-q,--query'] = 'query'
-ALL_VALID_OPTIONS['-t,--list-tiers'] = [True, False]
-ALL_VALID_OPTIONS['-c,--list-components'] = [True, False]
-ALL_VALID_OPTIONS['-r,--blueprint-refresh'] = [True, False]
+COMMON_VALID_OPTIONS['-p,--blueprint-generation-provider'] = 'generation_provider'
+COMMON_VALID_OPTIONS['-f,--blueprint-generation-framework'] = 'generation_framework'
+COMMON_VALID_OPTIONS['-i,--blueprint-generation-input-source'] = 'input-source'
+COMMON_VALID_OPTIONS['-s,--blueprint-generation-scenarios'] = 'generation_scenarious'
+COMMON_VALID_OPTIONS['-r,--blueprint-refresh'] = [True, False]
 
 
 @mock.patch('cot.command.query.query_backend')
-def test_input_valid(query_backend):
-    run_options_test(CliRunner(), query, ALL_VALID_OPTIONS, query_backend.run)
+def test_query_get_input(query_backend):
+    query_backend.run.return_value = {'query': None}
+    all_valid_options = copy.deepcopy(COMMON_VALID_OPTIONS)
+    all_valid_options['!-q,--query'] = 'query'
+    run_options_test(CliRunner(), get, all_valid_options, query_backend.run)
+
+
+@mock.patch('cot.command.query.query_backend')
+def test_query_list_tiers_input(query_backend):
+    query_backend.run.return_value = {'tiers': []}
+    run_options_test(CliRunner(), list_tiers, COMMON_VALID_OPTIONS, query_backend.run)
+
+
+@mock.patch('cot.command.query.query_backend')
+def test_query_list_components_input(query_backend):
+    query_backend.run.return_value = {'components': []}
+    run_options_test(CliRunner(), list_components, COMMON_VALID_OPTIONS, query_backend.run)
