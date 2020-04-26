fefbec47d12ac9fa70bb5b2708cf9369fec1a3e4
roleyfoley <roleyfoley@gmail.com>
2020-04-26 16:59:34 +1000

    feature: add unitlist generation command supports the generation of the unitlist output which provides a list of deployment units for a given segment

--- gen3-cli/cot/command/create/__init__.py
+++ gen3-cli/cot/command/create/__init__.py
@@ -4,6 +4,7 @@ from .template import template as template_cmd
 from .blueprint import blueprint as blueprint_cmd
 from .buildblueprint import buildblueprint as buildblueprint_cmd
 from .reference import reference as reference_cmd
+from .unitlist import unitlist as unitlist_cmd
 
 
 @cli.group('create')
@@ -17,3 +18,4 @@ group.add_command(template_cmd)
 group.add_command(blueprint_cmd)
 group.add_command(buildblueprint_cmd)
 group.add_command(reference_cmd)
+group.add_command(unitlist_cmd)
