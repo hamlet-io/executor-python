ec47621667f3ec44511220403b66f4d563398270
roleyfoley <roleyfoley@gmail.com>
2021-02-02 01:53:22 +0000

    refactor: create decorator for generation config
    Adds a decorator function which sets up the generation context for group
    commands. This contains the standard paramters which control generation
    across all of our commands which call the template backend

--- hamlet-cli/hamlet/command/common/context.py
+++ hamlet-cli/hamlet/command/common/context.py
@@ -1,4 +1,5 @@
 import click
+import functools
 
 
 class Generation():
@@ -10,3 +11,37 @@ class Generation():
 
 
 pass_generation = click.make_pass_decorator(Generation, ensure=True)
+
+def generation_config(func):
+    @functools.wraps(func)
+    @click.option(
+        '-p',
+        '--generation-provider',
+        help='plugins to load for output generation',
+        default=['aws'],
+        multiple=True,
+        show_default=True
+    )
+    @click.option(
+        '-f',
+        '--generation-framework',
+        help='output framework to use for output generation',
+        default='cf',
+        show_default=True
+    )
+    @click.option(
+        '-i',
+        '--generation-input-source',
+        help='source of input data to use when generating the output',
+        default='composite',
+        show_default=True
+    )
+    @click.pass_context
+    def decorator(ctx, generation_provider, generation_framework, generation_input_source, *args, **kwargs):
+        ctx.obj = Generation(
+            generation_provider=generation_provider if generation_provider else ctx.obj.generation_provider,
+            generation_framework=generation_framework if generation_framework else ctx.obj.generation_framework,
+            generation_input_source=generation_input_source if generation_input_source else ctx.obj.generation_input_source,
+        )
+        return func(*args, **kwargs)
+    return decorator
