ed034b205d878dc22ef821bf6e5582fe9125207f
roleyfoley <roleyfoley@gmail.com>
2021-02-02 06:46:03 +0000

    fix: remove old generation context

--- hamlet-cli/hamlet/command/visual/__init__.py
+++ hamlet-cli/hamlet/command/visual/__init__.py
@@ -13,16 +13,6 @@ from hamlet.backend import query as query_backend
 from hamlet.backend.common.exceptions import BackendException
 
 
-class Generation(object):
-    def __init__(self, generation_provider=None, generation_framework=None, generation_input_source=None):
-        self.generation_provider = generation_provider
-        self.generation_framework = generation_framework
-        self.generation_input_source = generation_input_source
-
-
-pass_generation = click.make_pass_decorator(Generation, ensure=True)
-
-
 @cli.group('visual')
 @generation_config
 def group():
