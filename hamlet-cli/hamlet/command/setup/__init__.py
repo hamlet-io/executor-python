297353a07a895a474de2fe796458840608f27aee
roleyfoley <roleyfoley@gmail.com>
2021-03-07 23:23:56 +0000

    feat: add setup command to cli
    adds the ${GENERATION_DIR}/setup.sh command to the cli
    This script is used to setup a hamlet workspace including the
    installation of plugins

---
+++ hamlet-cli/hamlet/command/setup/__init__.py
@@ -0,0 +1,23 @@
+import click
+
+from hamlet.command import root as cli
+from hamlet.command.common.exceptions import CommandError
+from hamlet.backend import setup as setup_backend
+from hamlet.backend.common.exceptions import BackendException
+
+@cli.command(
+    'setup',
+    short_help='',
+    context_settings=dict(
+        max_content_width=240
+    )
+)
+def setup(**kwargs):
+    '''
+    Sets up the local hamlet workspace
+    '''
+    try:
+        setup_backend.run(**kwargs, _is_cli=True)
+
+    except BackendException as e:
+        raise CommandError(str(e))
