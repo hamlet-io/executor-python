914a203b22e16342a45c41d4152ab4cfb8e16afb
roleyfoley <roleyfoley@gmail.com>
2021-03-10 01:56:24 +0000

    feat: add env options to setup

--- hamlet-cli/hamlet/command/setup/__init__.py
+++ hamlet-cli/hamlet/command/setup/__init__.py
@@ -2,7 +2,7 @@ from hamlet.command import root as cli
 from hamlet.command.common.exceptions import CommandError
 from hamlet.backend import setup as setup_backend
 from hamlet.backend.common.exceptions import BackendException
-
+from hamlet.command.common.config import pass_options
 
 @cli.command(
     'setup',
@@ -11,12 +11,17 @@ from hamlet.backend.common.exceptions import BackendException
         max_content_width=240
     )
 )
-def setup(**kwargs):
+@pass_options
+def setup(options, **kwargs):
     '''
     Sets up the local hamlet workspace
     '''
+    args = {
+        **options.opts,
+        **kwargs
+    }
     try:
-        setup_backend.run(**kwargs, _is_cli=True)
+        setup_backend.run(**args, _is_cli=True)
 
     except BackendException as e:
         raise CommandError(str(e))
