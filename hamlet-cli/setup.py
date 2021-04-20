9d28511f0205fdce4beb12d6c6b5f9e6ee89cd13
roleyfoley <roleyfoley@gmail.com>
2021-04-20 09:10:26 +0000

    refactor: set inital prerelease

--- hamlet-cli/setup.py
+++ hamlet-cli/setup.py
@@ -24,7 +24,7 @@ class InstallCommand(install):
 
 setup(
     name='hamlet-cli',
-    version='7.0.5',
+    version='7.0.6-dev0',
     author='Hamlet',
     url="https://github.com/hamlet-io/executor-python",
     long_description=long_description,
