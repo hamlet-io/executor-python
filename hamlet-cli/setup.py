6eddd66f2411047d29b6e41cf9fa586cbcbb603d
roleyfoley <roleyfoley@users.noreply.github.com>
2021-05-03 05:58:45 +0000

    chore: release bump [skip actions]

--- hamlet-cli/setup.py
+++ hamlet-cli/setup.py
@@ -24,7 +24,7 @@ class InstallCommand(install):
 
 setup(
     name='hamlet-cli',
-    version='7.0.6-dev3',
+    version='7.0.6-dev4',
     author='Hamlet',
     url="https://github.com/hamlet-io/executor-python",
     long_description=long_description,
