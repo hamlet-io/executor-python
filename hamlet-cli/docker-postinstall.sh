bea2e95c89fb4974fe4b49b02714c2e8f9bd569d
roleyfoley <roleyfoley@gmail.com>
2021-05-11 13:49:18 +0000

    fix: include setup packages in test

--- hamlet-cli/docker-postinstall.sh
+++
@@ -1,7 +0,0 @@
-# stash files to prevent deletion when host volume mounts
-# useful when you want to break stuff using your favorite editor
-# which is not vim or emac.
-echo "Moving /opt/hamlet => /hamlet to prevent overriding by volume"
-mv /opt/hamlet /hamlet
-echo "Current /hamlet content"
-tree /hamlet -L 2
