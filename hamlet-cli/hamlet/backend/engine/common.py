fe860a0ae40c7c36567448f4d3ba1e6fb3136053
roleyfoley <roleyfoley@gmail.com>
2021-06-04 01:01:25 +0000

    fix: handling of default and global engines
    Updates the cli setup process to handle the difference between a one
    time use of an engine and the inital setup of the global default engine.
    This simplifies the setup process and allows the cli to handle a
    sensible default to get people going

--- hamlet-cli/hamlet/backend/engine/common.py
+++ hamlet-cli/hamlet/backend/engine/common.py
@@ -6,3 +6,4 @@ ENGINE_STORE_DIR = os.path.join(HAMLET_HOME_DIR, 'engine')
 ENGINE_STORE_ENGINES_DIR = os.path.join(ENGINE_STORE_DIR, 'engines')
 ENGINE_STORE_ENGINE_STATE_FILENAME = 'engine_state.json'
 ENGINE_GLOBAL_NAME = '_global'
+ENGINE_DEFAULT_GLOBAL_ENGINE = 'unicycle'
