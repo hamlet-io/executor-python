73c0aa744275294dea50ee2dae45fc089dc0fd02
roleyfoley <roleyfoley@gmail.com>
2021-07-05 09:49:54 +0000

    refactor: backend automation tasks
    Rework the automation tasks from development and testing
    - Adds loading of base properties for registries and account details
    - Adds class specific parameters for each tasks
    - renames manage images to upload-images inline with the actual action
    performed
    - remove commands which aren't required

--- hamlet/backend/automation_tasks/base.py
+++ hamlet/backend/automation_tasks/base.py
@@ -1,32 +1,51 @@
 import os
+import tempfile
 from abc import ABC
 
+from hamlet.backend.automation.properties_file import get_automation_properties
 from configparser import ConfigParser
+
 
 class AutomationRunner(ABC):
     '''
     Executes an automation based task
     '''
 
-    def __init__(self):
-        self._context_env = {}
+    def __init__(self,  **kwargs):
+        self._context_env = kwargs
         self._script_list = []
 
     @staticmethod
-    def _load_properties_to_context(self, properties_file):
-        config = ConfigParser()
+    def _load_properties_to_context(properties_file):
 
-        s_config= open(properties_file, 'r').read()
-        s_config=f"[ini]\n{s_config}"
+        itemDict = {}
 
-        config.read_string(s_config)
-        items=config.items('ini')
-        itemDict={}
-        for key,value in items:
-            itemDict[key]=value
-        self._context_env = { **self._context_env, **itemDict}
+        if os.path.isfile(properties_file):
+            config = ConfigParser(strict=False)
+
+            s_config= open(properties_file, 'r').read()
+            s_config=f"[ini]\n{s_config}"
+
+            config.read_string(s_config)
+            items=config.items('ini')
+            for key,value in items:
+                itemDict[key]=value
+
+        return itemDict
 
     def run(self):
-        for script in self._script_list:
-            script['func']({ 'env': self._context_env, **script['args']})
-            self._load_properties_to_context(os.path.join(self.work_dir, 'context.properties'))
+
+        self._context_env['AUTOMATION_PROVIDER'] = 'hamletcli'
+
+        automation_properties = get_automation_properties(**self._context_env)
+        self._context_env.update(automation_properties)
+
+        with tempfile.TemporaryDirectory() as tmp_dir:
+
+            self._context_env['AUTOMATION_DATA_DIR'] = tmp_dir
+
+            for script in self._script_list:
+
+                script['func'](env=self._context_env, **script['args'])
+                script_context_envs = self._load_properties_to_context(os.path.join(tmp_dir, 'context.properties'))
+                self._context_env = { **self._context_env, **script_context_envs }
