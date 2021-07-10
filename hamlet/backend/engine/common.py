adb282d8d42d13afe015a55eae7cdd54075a9234
roleyfoley <roleyfoley@gmail.com>
2021-07-10 02:46:38 +0000

    feat: make the train engine the default
    Makes the train engine the default engine selected if no other global
    engines are found

--- hamlet/backend/engine/common.py
+++ hamlet/backend/engine/common.py
@@ -4,7 +4,7 @@ from hamlet.env import HAMLET_HOME_DIR
 
 ENGINE_STORE_DEFAULT_DIR = os.path.join(HAMLET_HOME_DIR, "engine")
 ENGINE_GLOBAL_NAME = "_global"
-ENGINE_DEFAULT_GLOBAL_ENGINE = "tram"
+ENGINE_DEFAULT_GLOBAL_ENGINE = "train"
 ENGINE_STATE_FILE_NAME = "engine_state.json"
 
 ENGINE_STATE_VERSION = "1.0.1"
