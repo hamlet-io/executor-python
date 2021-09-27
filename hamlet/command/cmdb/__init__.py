64551a098bd2bf0a8b6d941f44e1fb1cb389ecea
roleyfoley <roleyfoley@gmail.com>
2021-09-27 10:44:36 +0000

    feat: extend commit message support
    - extends the support for saving a commit message to include the type and
    description of the update
    - formats the commit message to align with the requirements of the bash
    runner backend

--- hamlet/command/cmdb/__init__.py
+++ hamlet/command/cmdb/__init__.py
@@ -27,30 +27,77 @@ def group():
     help="Include product repositories in save",
 )
 @click.option(
+    "--commit-type",
+    help="The type of commit to assign these changes to",
+    default="cd",
+    type=click.Choice(
+        [
+            "cd",
+            "feat",
+            "chore",
+            "fix",
+            "refactor",
+        ]
+    ),
+)
+@click.option(
+    "-d",
+    "--commit-description",
+    default="deploy_updates",
+)
+@click.option(
+    "-m",
     "--commit-message",
     required=True,
     help="The commit message for these changes",
 )
 @click.option(
-    "--reference",
+    "-b",
+    "--branch",
+    default="main",
     help="The repository reference to commit changes on",
 )
 @click.option(
     "--tag",
     help="The name of a tag to apply to the commit",
 )
+@click.option("--push/--no-push", default=True, help="Push committed changes")
 @exceptions.backend_handler()
 @config.pass_options
-def commit_changes(opts, accounts, products, commit_message, reference, tag, **kwargs):
+def commit_changes(
+    opts,
+    accounts,
+    products,
+    commit_type,
+    commit_description,
+    commit_message,
+    branch,
+    tag,
+    push,
+    **kwargs,
+):
     """
     Commit changes made to all CMDBs to their repositories
     """
 
+    message = [
+        f"cctype={commit_type}",
+        f"ccdesc={commit_description}",
+        f"msg={commit_message}",
+    ]
+
+    if accounts:
+        message.append(f"account={opts.account}")
+
+    if products:
+        message.append(f"product={opts.product}")
+
     command_args = {
         "account_repos": accounts,
-        "commit_message": commit_message,
         "product_repos": products,
-        "reference": reference,
+        "commit_message": ", ".join(message),
+        "reference": branch,
+        "defer_push": not push,
         "tag": tag,
     }
 
