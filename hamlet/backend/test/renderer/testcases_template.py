b8121c8456511d25ba3a32866ca7d8af422d811d
roleyfoley <roleyfoley@gmail.com>
2021-10-02 14:32:46 +0000

    feat: extend testing tooling
    - replace cfn_nag with checkov as testing tools as testing tools
    - adds support for ignoring checks on cfn_lint

--- hamlet/backend/test/renderer/testcases_template.py
+++ hamlet/backend/test/renderer/testcases_template.py
@@ -22,7 +22,7 @@ def prepare_context(cases):
     cfn_lint_test = False
     cfn_structure_test = False
     json_structure_test = False
-    cfn_nag_test = False
+    checkov_test = False
     prepared_cases = list()
     ordered_casenames = list(cases.keys())
     ordered_casenames.sort()
@@ -34,14 +34,14 @@ def prepare_context(cases):
         if type == "cf":
             prepare_cf_testcase_context(data)
             cfn_lint_test = cfn_lint_test or data.get("cfn_lint", False)
-            cfn_nag_test = cfn_nag_test or data.get("cfn_nag", False)
+            checkov_test = checkov_test or data.get("checkov", False)
             cfn_structure_test = cfn_structure_test or data.get("cfn_structure", False)
             json_structure_test = json_structure_test or data.get(
                 "json_structure", False
             )
         prepared_cases.append((casename, data, type))
     return {
-        "cfn_nag_test": cfn_nag_test,
+        "checkov_test": checkov_test,
         "cfn_lint_test": cfn_lint_test,
         "json_validation_test": cfn_structure_test or json_structure_test,
         "cfn_structure_test": cfn_structure_test,
---
+++ hamlet/backend/test/templates/checkov_test_func_block.py
@@ -0,0 +1,52 @@
+# NOTE: this file must remain valid python file in order to perform tests on it
+
+
+def checkov_test(filename, framework, skip_checks=None):
+    import json
+    import subprocess
+
+    cmd_args = [
+        "checkov",
+        "--output",
+        "json",
+        "--framework",
+        framework,
+        "--quiet",
+        "-f",
+        filename,
+    ]
+
+    if skip_checks is not None:
+        cmd_args.append("--skip-check")
+        cmd_args.append(",".join(skip_checks))
+
+    cmd = " ".join(cmd_args)
+
+    result = subprocess.run(
+        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf8"
+    )
+
+    if result.returncode == 1:
+        result = json.loads(result.stdout)
+        if result["summary"]["failed"] >= 0:
+            failure_details = []
+            for detail in result["results"]["failed_checks"]:
+                failure_details.append(
+                    {
+                        k: v
+                        for k, v in detail.items()
+                        if k
+                        in [
+                            "check_name",
+                            "check_id",
+                            "check_class",
+                            "resource",
+                            "guideline",
+                            "check_result",
+                        ]
+                    }
+                )
+            raise AssertionError(json.dumps(failure_details, indent=4))
+
+    if result.returncode >= 1:
+        raise Exception(result.stderr)
