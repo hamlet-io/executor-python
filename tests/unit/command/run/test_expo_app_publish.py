e64d1471f1a5f14d85531cf198594be6fd67cfda
roleyfoley <roleyfoley@gmail.com>
2021-10-28 23:11:45 +0000

    feat: extend expo publish command options
    - Aligns the available options in the cli with the commands avilable
    within the bash based backend script

--- tests/unit/command/run/test_expo_app_publish.py
+++ tests/unit/command/run/test_expo_app_publish.py
@@ -11,13 +11,14 @@ from tests.unit.command.test_option_generation import (
 ALL_VALID_OPTIONS = collections.OrderedDict()
 ALL_VALID_OPTIONS["!-u,--deployment-unit"] = "unit"
 ALL_VALID_OPTIONS["-s,--run-setup"] = [True, False]
-ALL_VALID_OPTIONS["-t,--binary-expiration"] = 1000
 ALL_VALID_OPTIONS["-f,--force-binary-build"] = [True, False]
 ALL_VALID_OPTIONS["-m,--submit-binary"] = [True, False]
-ALL_VALID_OPTIONS["-o,--disable-ota"] = [True, False]
-ALL_VALID_OPTIONS["-b,--binary-build-process"] = "turtle"
-ALL_VALID_OPTIONS["-q,--qr-build-formats"] = "formats"
-
+ALL_VALID_OPTIONS["-b,--binary-build-process"] = "fastlane"
+ALL_VALID_OPTIONS["-l,--build-logs"] = [True, False]
+ALL_VALID_OPTIONS["-e,--environment-badge"] = [True, False]
+ALL_VALID_OPTIONS["-d,--environment-badge-content"] = "environment"
+ALL_VALID_OPTIONS["-n,--node-package-manager"] = "yarn"
+ALL_VALID_OPTIONS["-v,--app-version-source"] = "cmdb"
 
 @mock.patch("hamlet.command.run.expo_app_publish.run_expo_app_publish_backend")
 def test_input_valid(run_expo_app_publish_backend):
@@ -39,5 +40,5 @@ def test_input_validation(run_expo_app_publish_backend):
         {
             "-u": "unit",
         },
-        [("-t", "not_an_int", 10)],
+        [],
     )
