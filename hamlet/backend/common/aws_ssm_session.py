c5fbed3565ad8378cecb19245cf19554c749c406
roleyfoley <roleyfoley@gmail.com>
2022-01-08 14:23:57 +1100

    feat: add support for ssm sessions
    - Adds a standard wrapper for running ssm sessions using the AWS
    SSM plugin for aws cli as a backend to handle websocket connection
    - Adds support for using ecs_run_command which uses ssm sessions to
    run interactive commands on ECS hosted containers

---
+++ hamlet/backend/common/aws_ssm_session.py
@@ -0,0 +1,99 @@
+import json
+import contextlib
+import sys
+import signal
+import boto3
+import errno
+from subprocess import check_call
+
+
+@contextlib.contextmanager
+def ignore_user_entered_signals():
+    """
+    Ignores user entered signals to avoid process getting killed.
+    """
+    if sys.platform == "win32":
+        signal_list = [signal.SIGINT]
+    else:
+        signal_list = [signal.SIGINT, signal.SIGQUIT, signal.SIGTSTP]
+    actual_signals = []
+    for user_signal in signal_list:
+        actual_signals.append(signal.signal(user_signal, signal.SIG_IGN))
+    try:
+        yield
+    finally:
+        for sig, user_signal in enumerate(signal_list):
+            signal.signal(user_signal, actual_signals[sig])
+
+
+def invoke(
+    session_id,
+    stream_url,
+    token_value,
+    parameters,
+    aws_access_key_id,
+    aws_secret_access_key,
+    aws_session_token,
+    region_name,
+    auth_profile_name="",
+):
+    """
+    Invokes an SSM interactice session using session details
+    that have been created in another process
+    """
+
+    session = boto3.Session(
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        aws_session_token=aws_session_token,
+        region_name=region_name,
+    )
+    ssm = session.client("ssm")
+    endpoint_url = ssm.meta.endpoint_url
+
+    ssm_session = {
+        "SessionId": session_id,
+        "StreamUrl": stream_url,
+        "TokenValue": token_value,
+    }
+
+    try:
+        # ignore_user_entered_signals ignores these signals
+        # because if signals which kills the process are not
+        # captured would kill the foreground process but not the
+        # background one. Capturing these would prevents process
+        # from getting killed and these signals are input to plugin
+        # and handling in there
+
+        with ignore_user_entered_signals():
+            # call executable with necessary input
+            check_call(
+                [
+                    "session-manager-plugin",
+                    json.dumps(ssm_session),
+                    region_name,
+                    "StartSession",
+                    auth_profile_name,
+                    json.dumps(parameters),
+                    endpoint_url,
+                ]
+            )
+        return 0
+
+    except OSError as ex:
+        if ex.errno == errno.ENOENT:
+
+            # start-session api call returns response and starts the
+            # session on ssm-agent and response is forwarded to
+            # session-manager-plugin. If plugin is not present, terminate
+            # is called so that service and ssm-agent terminates the
+            # session to avoid zombie session active on ssm-agent for
+            # default self terminate time
+            ssm.terminate_session(SessionId=session_id)
+            raise ValueError(
+                (
+                    "AWS Session manager plugin could not be found: "
+                    "see https://docs.aws.amazon.com/systems-manager/"
+                    "latest/userguide/session-manager-working-with-install-plugin.html"
+                )
+            )
---
+++ hamlet/backend/contract/tasks/aws_ecs_run_command/__init__.py
@@ -0,0 +1,78 @@
+import boto3
+from hamlet.backend.common import aws_ssm_session
+
+
+def build_ssm_request_paramaters(task_details, container_name):
+    cluster_name = task_details["clusterArn"].split("/")[-1]
+    task_id = task_details["taskArn"].split("/")[-1]
+
+    container_details = list(
+        filter(
+            lambda container: container["name"] == container_name,
+            task_details["containers"],
+        )
+    )[0]
+
+    ssm_request_params = {
+        "Target": f"ecs:{cluster_name}_{task_id}_{container_details['runtimeId']}"
+    }
+    return ssm_request_params
+
+
+def run(
+    ClusterArn=None,
+    TaskArn=None,
+    ContainerName=None,
+    Command=None,
+    Region=None,
+    AWSAccessKeyId=None,
+    AWSSecretAccessKey=None,
+    AWSSessionToken=None,
+    env={},
+):
+    """
+    Run an interactive command on aws ecs task
+    Uses ECS SSM integration to provide interactive console through the
+    Session Manager plugin for aws cli
+    """
+    session = boto3.Session(
+        aws_access_key_id=AWSAccessKeyId,
+        aws_secret_access_key=AWSSecretAccessKey,
+        aws_session_token=AWSSessionToken,
+        region_name=Region,
+    )
+
+    ecs = session.client("ecs")
+
+    execute_params = {
+        "cluster": ClusterArn,
+        "task": TaskArn,
+        "command": Command,
+        "interactive": True,
+    }
+
+    if ContainerName is not None:
+        execute_params["container"] = ContainerName
+
+    command = ecs.execute_command(**execute_params)
+
+    task_details = ecs.describe_tasks(cluster=ClusterArn, tasks=[TaskArn])
+    task_details = list(
+        filter(lambda task: task["taskArn"] == TaskArn, task_details["tasks"])
+    )[0]
+
+    if ContainerName is None:
+        ContainerName = task_details["containers"][0]["name"]
+
+    aws_ssm_session.invoke(
+        session_id=command["session"]["sessionId"],
+        stream_url=command["session"]["streamUrl"],
+        token_value=command["session"]["tokenValue"],
+        parameters=build_ssm_request_paramaters(task_details, ContainerName),
+        aws_access_key_id=AWSAccessKeyId,
+        aws_secret_access_key=AWSSecretAccessKey,
+        aws_session_token=AWSSessionToken,
+        region_name=Region,
+    )
+
+    return {}
