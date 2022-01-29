19252b6db088845a41aeae26ce31cd70c68deb65
roleyfoley <roleyfoley@gmail.com>
2022-01-29 14:27:21 +1100

    feat: update sentry_release command
    - Align the sentry release command with the latest options for the
    backend

--- hamlet/backend/run/sentry_release/__init__.py
+++ hamlet/backend/run/sentry_release/__init__.py
@@ -2,10 +2,12 @@ from hamlet.backend.common import runner
 
 
 def run(
+    deployment_unit=None,
+    deployment_group=None,
     sentry_source_map_s3_url=None,
     sentry_url_prefix=None,
     sentry_release_name=None,
-    run_setup=None,
+    app_type=None,
     log_level=None,
     root_dir=None,
     tenant=None,
@@ -20,7 +22,9 @@ def run(
         "-m": sentry_source_map_s3_url,
         "-p": sentry_url_prefix,
         "-r": sentry_release_name,
-        "-s": run_setup,
+        "-u": deployment_unit,
+        "-g": deployment_group,
+        "-a": app_type,
     }
     env = {
         "GENERATION_LOG_LEVEL": log_level,
