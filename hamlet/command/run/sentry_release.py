6202c514d7f66528b8af773621ace115d9de3bb2
roleyfoley <roleyfoley@gmail.com>
2022-01-29 14:27:21 +1100

    feat: update sentry_release command
    - Align the sentry release command with the latest options for the
    backend

--- hamlet/command/run/sentry_release.py
+++ hamlet/command/run/sentry_release.py
@@ -6,16 +6,36 @@ from hamlet.command.common.config import pass_options
 
 @click.command(
     "sentry-release",
-    short_help="Upload sourcemap files to sentry for a specific release",
+    short_help="",
     context_settings=dict(max_content_width=240),
+)
+@click.option(
+    "-u",
+    "--deployment-unit",
+    required=True,
+    help="The deployment unit of the moobile app",
+)
+@click.option(
+    "-g",
+    "--deployment-group",
+    default="application",
+    help="The deployment group of the moobile app",
 )
 @click.option(
     "-m", "--sentry-source-map-s3-url", required=True, help="s3 link to sourcemap files"
 )
-@click.option("-p", "--sentry-url-prefix", help="prefix for sourcemap files")
-@click.option("-r", "--sentry-release-name", help="sentry release name", required=True)
 @click.option(
-    "-s", "--run-setup", help="run setup installation to prepare", is_flag=True
+    "-a",
+    "--app-type",
+    type=click.Choice(["", "react-native"]),
+    help="The application framework being used",
+)
+@click.option("-p", "--sentry-url-prefix", help="prefix for sourcemap files")
+@click.option(
+    "-r",
+    "--sentry-release-name",
+    help="release name",
+    required=True,
 )
 @exceptions.backend_handler()
 @pass_options
@@ -25,5 +45,4 @@ def sentry_release(options, **kwargs):
     """
 
     args = {**options.opts, **kwargs}
-
     run_sentry_release_backend.run(**args, _is_cli=True)
--- tests/unit/command/run/test_sentry_release.py
+++ tests/unit/command/run/test_sentry_release.py
@@ -5,10 +5,12 @@ from hamlet.command.run.sentry_release import sentry_release as run_sentry_relea
 from tests.unit.command.test_option_generation import run_options_test
 
 ALL_VALID_OPTIONS = collections.OrderedDict()
+ALL_VALID_OPTIONS["!-u,--deployment-unit"] = "deployment_unit"
 ALL_VALID_OPTIONS["!-m,--sentry-source-map-s3-url"] = "url"
 ALL_VALID_OPTIONS["!-r,--sentry-release-name"] = "release_name"
+ALL_VALID_OPTIONS["-g,--deployment-group"] = "deployment_group"
 ALL_VALID_OPTIONS["-p,--sentry-url-prefix"] = "url_prefix"
-ALL_VALID_OPTIONS["-s,--run-setup"] = [True, False]
+ALL_VALID_OPTIONS["-a,--app-type"] = "react-native"
 
 
 @mock.patch("hamlet.command.run.sentry_release.run_sentry_release_backend")
