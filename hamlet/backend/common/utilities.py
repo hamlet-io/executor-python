cb84cd9907eeeb2bf930b24073216a41e972f914
roleyfoley <roleyfoley@gmail.com>
2022-04-04 20:34:18 +1000

    refactor: distutils strtobool
    - Replaces strtobool with a local version of the function
    
    distutils has been deprecated and will be removed in new versions
    of python

--- hamlet/backend/common/utilities.py
+++ hamlet/backend/common/utilities.py
@@ -1,12 +1,22 @@
-from distutils.util import strtobool
+from typing import Any
 
 
-def to_bool(value, default):
+def str_to_bool(value: Any) -> bool:
+    """
+    Return whether the provided string (or any value really) represents true. Otherwise false.
+    Just like plugin server stringToBoolean.
+    """
+    if not value:
+        return False
+    return str(value).lower() in ("y", "yes", "t", "true", "on", "1")
+
+
+def to_bool(value: Any, default) -> bool:
     """
     Converts string or None to bool
     """
     if isinstance(value, str):
-        return strtobool(value)
+        return str_to_bool(value)
     elif value is None:
         return default
     else:
