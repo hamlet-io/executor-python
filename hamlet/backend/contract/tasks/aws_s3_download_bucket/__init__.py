65c4e01fa440daecbf4d5340df5f5e1a516aa8a6
roleyfoley <roleyfoley@gmail.com>
2022-04-04 20:29:42 +1000

    feat: aws s3 copy rm tasks
    - adds tasks for aws s3 related actions
      - emptying a bucket included versions
      - Copying the contents of a bucket locally

---
+++ hamlet/backend/contract/tasks/aws_s3_download_bucket/__init__.py
@@ -0,0 +1,46 @@
+import boto3
+import os
+
+
+def run(
+    BucketName,
+    Prefix,
+    LocalPath,
+    Region=None,
+    AWSAccessKeyId=None,
+    AWSSecretAccessKey=None,
+    AWSSessionToken=None,
+    env={},
+):
+    """
+    Download the objects matching a prefix to a local directory from an S3 Bucket
+    """
+
+    session = boto3.Session(
+        aws_access_key_id=AWSAccessKeyId,
+        aws_secret_access_key=AWSSecretAccessKey,
+        aws_session_token=AWSSessionToken,
+        region_name=Region,
+    )
+
+    s3 = session.resource("s3")
+    bucket = s3.Bucket(BucketName)
+    object_count = 0
+
+    for object in bucket.objects.filter(Prefix=Prefix):
+        object_count += 1
+        local_path = os.path.join(
+            os.path.abspath(LocalPath),
+            object.key[len(os.path.commonpath([Prefix, object.key])):]
+        )
+
+        os.makedirs(os.path.dirname(local_path), exist_ok=True)
+        s3.Object(object.bucket_name, object.key).download_file(local_path)
+
+    return {
+        "Properties": {
+            "object_count": str(object_count),
+            "s3_path": f"s3://{BucketName}/" + Prefix,
+            "local_path": LocalPath
+        }
+    }
---
+++ hamlet/backend/contract/tasks/aws_s3_empty_bucket/__init__.py
@@ -0,0 +1,43 @@
+import boto3
+
+
+def run(
+    BucketName,
+    Prefix,
+    Region=None,
+    AWSAccessKeyId=None,
+    AWSSecretAccessKey=None,
+    AWSSessionToken=None,
+    env={},
+):
+    """
+    Remove all objects from an S3 bucket prefix
+    """
+
+    session = boto3.Session(
+        aws_access_key_id=AWSAccessKeyId,
+        aws_secret_access_key=AWSSecretAccessKey,
+        aws_session_token=AWSSessionToken,
+        region_name=Region,
+    )
+
+    s3 = session.resource("s3")
+    bucket = s3.Bucket(BucketName)
+    object_count = 0
+
+    if bucket.Versioning().status in ["Enabled", "Suspended"]:
+        for object in bucket.object_versions.filter(Prefix=Prefix):
+            object_count += 1
+            object.delete()
+
+    else:
+        for object in bucket.objects.filter(Prefix=Prefix):
+            object_count += 1
+            object.delete()
+
+    return {
+        "Properties": {
+            "object_count": str(object_count),
+            "s3_path": f"s3://{BucketName}/" + Prefix
+        }
+    }
