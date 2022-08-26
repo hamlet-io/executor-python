3be36503b64f690fac364188f4948d24b12facdc
roleyfoley <roleyfoley@gmail.com>
2022-08-26 13:30:17 +1000

    fix: remove use of transport from httpx client
    - Removes the custom transport option used for the container image
    client as it also disables the use of env proxy configuration
    - Creates a standard http client that should be used hamlet overall
    to ensure consistency in the use of the http client

---
+++ hamlet/backend/common/http_client.py
@@ -0,0 +1,27 @@
+from httpx import Client, Timeout
+
+
+class HTTPClient(Client):
+    """
+    A standard httpx client used in the hamlet cli
+    """
+
+    def __init__(
+        self,
+        *args,
+        http1: bool = True,
+        http2: bool = True,
+        timeout: Timeout(timeout=5.0, read=120.0),
+        follow_redirects: bool = True,
+        trust_env: bool = True,
+        **kwargs
+    ):
+        super().__init__(
+            *args,
+            http1,
+            http2,
+            timeout,
+            follow_redirects,
+            trust_env,
+            **kwargs
+        )
