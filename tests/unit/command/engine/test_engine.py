0cdba0a035d14d7c85ff73d24806c8fac892e5c4
roleyfoley <roleyfoley@gmail.com>
2023-09-28 17:17:52 +1000

    chore: foramtting cleanups

--- tests/unit/command/engine/test_engine.py
+++ tests/unit/command/engine/test_engine.py
@@ -3,13 +3,8 @@ from unittest import mock
 
 from click.testing import CliRunner
 
-from hamlet.command.engine import (
-    clean_engines,
-    describe_engine,
-    install_engine,
-    list_engines,
-    set_engine,
-)
+from hamlet.command.engine import (clean_engines, describe_engine,
+                                   install_engine, list_engines, set_engine)
 
 
 def test_list_engines(options):
